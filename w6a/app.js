ab98c655337d535eae85c901846ce4d74df04ce5
Shena Ellis <shena.marie11@gmail.com>
2026-02-10 13:07:42 -0800

    Step 1 complete

---
+++ w6a/app.js
@@ -0,0 +1,54 @@
+// So the browser loads the new file
+import * as orderHandler from "./order-handler.js";
+import * as priceCalculator from "./price-calculator.js"; 
+import * as resultsDisplay from "./results-display.js";
+// --- Part 1: Select the T-shirt from the summary div
+const orderFormEl = document.getElementById("order-form");
+const summaryDiv = document.getElementById('order-summary');
+// Step 3.2 The Array
+const orders = [];
+
+
+// These variables will change as the user interacts with the page.
+
+// --- Part 3.3 Update handleOrderSubmit
+    const handleOrderSubmit = function(event) {
+    event.preventDefault(); // Stop pagefrom reloading
+   
+    // 1) Get the form data object
+    const data = orderHandler.getOrderInputs();
+   
+    // 2) --- Calculate --- total price (returns {totalPrice: ...})
+    const calculatedPrice = priceCalculator.calculateTotal(data);
+
+    // 3) Timestamp object
+    const timestamp = { timestamp: new Date().toISOString() };
+
+    // 4) --- Merge --- into one object (The Spread Operator)
+    const newOrder = { ...data, ...calculatedPrice, ...timestamp };
+
+    // 5) --- Store --- new objects in orders array
+    orders.push(newOrder);
+
+    // 6) --- Verify --- the orders history increases
+    console.log(orders);
+
+    resultsDisplay.displayResults(newOrder);
+    
+    console.log('Order Received!', data);
+
+  }
+
+// 3. Initialize the app
+  const init = function () {
+    console.log("App Initialized");
+   orderFormEl.addEventListener("submit", handleOrderSubmit);
+};
+
+document.addEventListener('DOMContentLoaded', init);
+
+
+
+
+
+// Step 2 - added line 3
